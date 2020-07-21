63378c7ebb23bc85a7b698445eddcec152eb38fa
AmirBlackstack <amir.nisar@bkstack.com>
2020-07-21 06:07:15 +0000

    edit update

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,39 @@
+class TodosController < ApplicationController
+	def new
+		@todo = Todo.new
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		if @todo.save
+			flash[:notice] = "Todo was created successfully!"
+			redirect_to todo_path(@todo)
+		else
+			render "new"
+		end
+	end 
+
+	def show
+		@todo = Todo.find(params[:id])
+	end
+
+	def edit
+		@todo = Todo.find(params[:id])
+	end
+
+	def update
+		@todo = Todo.find(params[:id])
+		if @todo.update(todo_params)
+			flash[:notice] = "Todo was updated successfully!"
+			redirect_to todo_path(@todo)
+		else
+			rendeer "edit"
+		end
+	end
+
+	private
+
+		def todo_params
+			params.require(:todo).permit(:name, :description)
+		end
+end
