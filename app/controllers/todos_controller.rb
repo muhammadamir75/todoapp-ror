b3d6ecb4bf153c8803c0c20837a77b8197e29a05
AmirBlackstack <amir.nisar@bkstack.com>
2020-07-21 07:01:17 +0000

    todoapp completed!

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+	before_action :set_todo, only: [:edit, :show, :update, :destroy]
+
 	def new
 		@todo = Todo.new
 	end
@@ -14,15 +16,14 @@ class TodosController < ApplicationController
 	end 
 
 	def show
-		@todo = Todo.find(params[:id])
+
 	end
 
 	def edit
-		@todo = Todo.find(params[:id])
+
 	end
 
 	def update
-		@todo = Todo.find(params[:id])
 		if @todo.update(todo_params)
 			flash[:notice] = "Todo was updated successfully!"
 			redirect_to todo_path(@todo)
@@ -31,7 +32,21 @@ class TodosController < ApplicationController
 		end
 	end
 
+	def index
+		@todos = Todo.all
+	end
+
+	def destroy
+		@todo.destroy
+		flash[:notice] = "Todo was deleted successfully"
+		redirect_to todos_path
+	end
+
 	private
+
+		def set_todo
+			@todo = Todo.find(params[:id])
+		end
 
 		def todo_params
 			params.require(:todo).permit(:name, :description)
